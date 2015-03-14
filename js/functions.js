cc3c48f0eb331555b68c089e406139f8b2951700
ariparas <smartarima@gmail.com>
2015-03-13 22:33:26 -0700

    Design First Look
    This is the first look of our design. As of now Drop down menu option is
    added with the search functionality on the list of options.

---
+++ js/functions.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+	if( $(".chosen").length ) {
+		$(".chosen").chosen();
+		/* $.getJSON('http://pollutantapi-aaroncheng.rhcloud.com/category/index',function(data) {
+			console.log(data);
+		}); */
+	}
+});
